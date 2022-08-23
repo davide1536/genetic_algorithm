b216ccddc29cab639c0df8db8b5afd9a2f578f6d
davide1998 <davide.franzoso@student.unife.it>
2022-08-23 21:30:35 +0200

    experiments

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -1,4 +1,6 @@
+from cProfile import label
 from typing import Dict
+from unittest import result
 from numpy.random import randint
 from collections import defaultdict
 from Graph import Graph
@@ -8,6 +10,8 @@ from numpy.random import rand
 import math
 import os
 import numpy as np
+import timeit
+import matplotlib.pyplot as plt
 def checkNodePresence(node1, node2,g):
     if g.adj_list.get(node1) and g.adj_list.get(node2):
         return 0
@@ -60,8 +64,7 @@ def objective(path, g):
         node1 = path[i]
         node2 = path[i+1]
 
-        #print("node 1: ", node1)
-        #print("node2: ", node2)
+       
         outOfBound_flag = checkNodePresence(node1, node2, g)
 
         
@@ -84,8 +87,8 @@ def objective(path, g):
         else:
             return -1
 
-    #print("total cost:", totalCost)
-    #print("flag trovate (obstacle, absence): ",obstacle_flag, " ", absence_flag)
+    # print("total cost:", totalCost)
+    # print("flag trovate (obstacle, absence): ",obstacle_flag, " ", absence_flag)
 
     if obstacle_flag != 1 and absence_flag != 1 and outOfBound_flag != 1:
         fit = 1.0/totalCost
@@ -94,8 +97,8 @@ def objective(path, g):
         fit = -1
     
     #print("fit", fit)
-    if fit > 0:
-        print ("fit: ",fit)
+    # if fit > 0:
+    #     print ("fit: ",fit)
     
     return fit
 
@@ -150,12 +153,13 @@ def mutation(bitstring, r_mut, bit_len):
             #flip bit
             bitstring[i] = 1 - bitstring[i]
 
-def genetic_algorithm(objective, n_bits, n_iter, n_pop, r_cross, r_mut, graph, starting_point, ending_point):
+def genetic_algorithm(objective, n_bits, n_iter, n_pop, r_cross, r_mut, graph, starting_point, ending_point, bit_len):
     #initalize population
+    timePerIteration = 0
     pop = [randint(0,2,n_bits).tolist() for _ in range(n_pop)]
     
     cod_len = len(starting_point)
-    print("lunghezza codifica: ", cod_len)
+    #print("lunghezza codifica: ", cod_len)
 
     for x in pop:
         x[0:cod_len] = starting_point
@@ -165,7 +169,9 @@ def genetic_algorithm(objective, n_bits, n_iter, n_pop, r_cross, r_mut, graph, s
     
     best, best_eval = 0, objective(decode(pop[0], cod_len), graph)
     #main loop
+    bestGen = 0
     for gen in range(n_iter):
+        start = timeit.default_timer()
         #print("inizia la decodifica")
         decoded = [decode(p, cod_len) for p in pop]
         #print("finisce la decodifica")
@@ -173,8 +179,9 @@ def genetic_algorithm(objective, n_bits, n_iter, n_pop, r_cross, r_mut, graph, s
         #check for best solution
         for i in range(n_pop):
             if scores[i] > best_eval:
+                bestGen = gen
                 best, best_eval = pop[i], scores[i]
-                print(">%d, new best f(%s) = %.3f" % (gen,  pop[i], scores[i]))
+                #print(">%d, new best f(%s) = %.3f" % (gen,  pop[i], scores[i]))
         
         #select parents
         selected = [selection(pop, scores) for _ in range(n_pop)]
@@ -190,22 +197,26 @@ def genetic_algorithm(objective, n_bits, n_iter, n_pop, r_cross, r_mut, graph, s
         
         #print ("i figli sono: ", children)
         pop = children
+        end = timeit.default_timer()
+
+        timePerIteration += (end-start)
         #print("nuova popolazione: ", pop)
     
     cost = 1/best_eval
-    
-    return [best, best_eval, cost]
+    timePerIteration = timePerIteration / n_iter
+    print("best gen: ", bestGen)
+    return [best, best_eval, cost, bestGen, timePerIteration]
     
 
 
-def parsing(directory):
+def parsing(directory, length):
     for file in os.listdir(directory):
-        g, starting_point, ending_point, bit_len=create_graph(file)
+        g, starting_point, ending_point, bit_len=create_graph(file,length)
         return [g, starting_point, ending_point, bit_len]
 
 
 #inizializza grafo
-def create_graph(file):
+def create_graph(file,bit_len):
    
 
     weights = []
@@ -217,23 +228,24 @@ def create_graph(file):
     n_nodes = first_line[0]
     my_dict = {i: np.zeros(int(n_nodes)) for i in range(int(n_nodes))}
     lines = f.read().splitlines()
-    
+
+
     for line in lines:
         total_node.append(line.split()[0])
         total_node.append(line.split()[1])
-        my_dict[int(line.split()[0])-1][int(line.split()[1])-1] = int(line.split()[2])
-        my_dict[int(line.split()[1])-1][int(line.split()[0])-1] = int(line.split()[2])
+        my_dict[int(line.split()[0])][int(line.split()[1])] = int(line.split()[2])
+        my_dict[int(line.split()[1])][int(line.split()[0])] = int(line.split()[2])
     
-    print(my_dict)
+   
     nodes = list(dict.fromkeys(total_node))
-    nodes = [int(x)-1 for x in nodes]
+    nodes = [int(x) for x in nodes]
     nodes.sort()
-    print (nodes)
+    
     for i in range(len(nodes)):
         my_dict[i] = np.where(my_dict[i] == 0, -1,my_dict[i])
         my_dict[i][i] = 0.0
         weights.append(my_dict[i].tolist())
-    #print(weights)
+    
 
     adj_list = {}
 
@@ -257,9 +269,8 @@ def create_graph(file):
     
         adj_list.setdefault(node, [])
     #convert in binary the starting and the ending point of the graph
-    bit_len = 4
 
-    starting_point, ending_point = code(0,5, bit_len)
+    starting_point, ending_point = code(0,1, bit_len)
     # print(starting_point)
     # print(ending_point)
 
@@ -272,10 +283,13 @@ def create_graph(file):
         
             if weight == None:
                 Weight = weights[node2-1][node1-1]
+            
+            
             if (node1 == 1 and node2 == 2) or (node1 == 2 and node2 == 1):
                 obstacle = 1
-            # elif (node1 == 2 and node2 == 1) or (node1 == 1 and node2 == 2):
-            #     obstacle = 1
+            elif (node1 == 0 and node2 == 1) or (node1 == 1 and node2 == 0):
+                obstacle = 1
+           
             else:
                 obstacle = 0
             
@@ -285,17 +299,113 @@ def create_graph(file):
 
     g.adj_list = adj_list
     return [g, starting_point, ending_point,bit_len]
+
+def main(pop, bit_len):
+    outcome = 0
+    n_obstacles = 2
+    g, starting_point, ending_point,bit_len= parsing('dataset/', bit_len)
+    iterations = 1000
+    # g, starting_point, ending_point = create_graph()
     
-n_obstacles = 2
-g, starting_point, ending_point,bit_len= parsing('dataset/')
-# g, starting_point, ending_point = create_graph()
-best, score, cost = genetic_algorithm(objective, int((5)*bit_len), 300, 500, 0.9, 1.0/(n_obstacles+2)*bit_len, g, [int(x) for x in starting_point], [int(x) for x in ending_point])
-if best == 0:
-    print("percorso valido non trovato")
-else:
-    decoded = decode(best, bit_len)
-    decoded_path = get_path(decoded, g)
-    print("solution: ",decoded_path, "score: ", score, "cost:", cost)
+    best, score, cost, gen, timePerIteration = genetic_algorithm(objective, int((n_obstacles+2)*bit_len), iterations, pop, 0.9, 1.0/(n_obstacles+2)*bit_len, g, [int(x) for x in starting_point], [int(x) for x in ending_point], bit_len)
+    
+    if best == 0:
+        print("percorso valido non trovato")
+        gen = iterations
+    else:
+        decoded = decode(best, bit_len)
+        decoded_path = get_path(decoded, g)
+        outcome = 1
+        print("solution: ",decoded_path, "score: ", score, "cost:", cost)
+
+    return gen, timePerIteration, outcome
+
+def drawGraphs(totalIterations, totalTimesPerIteration, totalTimes, totalOutcomes, pop, length):
+    plt.plot(pop,  totalTimesPerIteration[0], label = "time per iteration")
+    plt.legend()
+    plt.title("times per iteration")
+    plt.xlabel("population size")
+    plt.ylabel("time per iteration")
+    plt.show()
+    for i,l in enumerate(length):
+        
+        plt.plot(pop, totalIterations[i], label ="iterations")
+        plt.legend()
+        plt.title("Number of iterations to get the solution bit len = %i" % l )
+        plt.ylabel("number iterations")
+        plt.xlabel("population size")
+        plt.show()
+
+        plt.plot(pop, totalTimes[i], label="total time")
+        plt.legend()
+        plt.title("Total time spent by the algorithm in order to get the solution len = %i" % l)
+        plt.xlabel("population size")
+        plt.ylabel("time")
+
+        plt.show()
+
+
+            
+
+
+def experiments():
+    
+    n_pop = [2, 10, 30, 50, 100]
+    n_experiments = 20
+    bit_len = [2,4,6]
+    matrixOutcomes = []
+    matrixIterations = []
+    matrixTotalTimes = []
+    matrixTimePerIteration = []
+    for i,length in enumerate(bit_len):
+        iterations = []
+        totalTimes = []
+        timesPerIteration = []
+        outcomes = []
+        for j,pop in enumerate(n_pop):
+            outcome = 0
+            iteration = 0
+            totalTime = 0
+            timePerIteration = 0
+            for _ in range(n_experiments):
+                results = main(pop, length)
+                iteration += results[0] + 1
+                timePerIteration += results[1]
+                totalTime += iteration * timePerIteration
+                outcome += results[2]
+
+        
+            percentageOutcome = outcome/n_experiments
+            iteration = round(iteration/n_experiments)
+            totalTime = totalTime/n_experiments
+            timePerIteration = timePerIteration/n_experiments  
+            outcomes.append(percentageOutcome)  
+            iterations.append(iteration)
+            totalTimes.append(totalTime)
+            timesPerIteration.append(timePerIteration)
+        
+        matrixIterations.append(iterations)
+        matrixOutcomes.append(outcomes)
+        matrixTimePerIteration.append(timesPerIteration)
+        matrixTotalTimes.append(totalTimes)
+
+    
+    print(len(matrixIterations))
+    print(len(matrixIterations[0]))
+    
+    # for i in range(len(n_pop)):
+    #     print("pop: ",n_pop[i])
+    #     for j in range(len(bit_len)):
+    #         print("bit len: ", bit_len[j])
+    #         print("iterations done: ",matrixIterations[i][j])
+    #         print("total times elapsed: ", matrixTotalTimes[i][j])
+    #         print("times per iteration: ", matrixTimePerIteration[i][j])
+    #         print("percentage of outcomes: ", matrixOutcomes[i][j])
+
+
+    drawGraphs(matrixIterations, matrixTimePerIteration, matrixTotalTimes, matrixOutcomes, n_pop, bit_len)
+
+experiments()
 
 
 
